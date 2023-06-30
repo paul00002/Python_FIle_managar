0a2904e02c84e51bcd4176d52d2be45cf6c46165
paul00002 <paul00002>
2023-06-30 22:45:48 +0200

    Signed-off-by: paul00002 <paul00002> Dir and file 60%

--- Functions.py
+++ Functions.py
@@ -3,6 +3,8 @@ import os
 from  glob import glob
 from typing import Any
 import pandas as pd
+import pandasql as ps
+import duckdb
 user=os.environ['USERPROFILE']
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') 
        
@@ -30,51 +32,62 @@ class File():
             return True
          else:
             return False
+    def open(self):
+        os.system(os.path.join(file_m.rootpath,'"'+file_m.namefile+'"'))
         
 class Director():
     def __init__(self,path) -> None:
         self.path=path
         self.namefile=os.path.basename(path)
       
-    def load(self):
-        return pd.DataFrame(list(self.type_path()),columns=["Name_file","Object","Type"])
-          
-    def delete(self,name):
-        if name in self.Files:
-          os.system('del {Pfile}'.format(Pfile=self.Files[name].path))
-          if os.path.exists(self.Files[name].path):
+    def Load(self):
+        self.load=self.Type_path()
+        self.files={x:z for x,y,z,r in self.load}
+        return self
+        
+    def Pd_DataFrame(self):
+        return pd.DataFrame(list(self.Type_path()),columns=["Name_file","Object","Type","Root"])
+    
+    def Delete(self,name):
+        if name in self.files:
+          os.system('del {Pfile}'.format(Pfile=self.files[name].path))
+          if os.path.exists(self.files[name].path):
               return False
           else:
               return True
           
     def Move(self,name,new_path):
         if  os.path.exists(new_path):
-            os.system('move {Pfile} {Npath}'.format(Pfile=self.Files[name].path,Npath=new_path))
+            os.system('move {Pfile} {Npath}'.format(Pfile=self.files[name].path,Npath=new_path))
             return True 
         else:
             return False
     
-    def copy(self,name,new_path):
+    def Copy(self,name,new_path):
         if  os.path.exists(new_path):
-            os.system('copy {Pfile} {Npath}'.format(Pfile=self.Files[name].path,Npath=new_path))
+            os.system('copy {Pfile} {Npath}'.format(Pfile=self.files[name].path,Npath=new_path))
             return True 
         else:
             return False
     
-    def exists(self,name):
-        if name in self.Files:
+    def Exists(self,name):
+        if name in self.files:
             return True
         else: 
             return False
 
-    def type_path(self):
+    def Type_path(self):
         for x in os.listdir(self.path):
             try: 
                 x.split('.')[1]
-                yield x,File(os.path.join(self.path,x)),"File",
+                yield x,File(os.path.join(self.path,x)),"File",x.split('.')[1]
             except:
-                yield x,Director(os.path.join(self.path,x)),"Director"
+                yield x,Director(os.path.join(self.path,x)),"Director",None
    
 if __name__=='__main__':
-   dire=Director(desktop).load()
-   fi=dire['Object'][0]
+   dire=Director(desktop).Load()
+   pf=dire.Pd_DataFrame()
+   files=pf.loc[pf['Type'].isin(["File"]) & pf['Root'].isin(["lnk"])].iloc[:,0:2]
+   print(duckdb.query("SELECT * FROM pf").df())
+   #file_m=files['Object'][20]
+   #print(file_m.open())
